46b246419ada2e3e649729d50fbd003a1e3fc658
Serhat Yılmaz <yilmaz.srhat@gmail.com>
2017-10-01 23:48:28 +0300

    Capture / Ion Native

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
+import { MediaCapture } from '@ionic-native/media-capture';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
@@ -28,7 +30,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    MediaCapture
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { MediaCapture , MediaFile , CaptureError } from '@ionic-native/media-capture';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  videoUrl:any;
+
+  constructor(public navCtrl: NavController , private mediaCapture: MediaCapture) {
 
   }
 
+  startCapture(){
+    this.mediaCapture.captureVideo()
+    .then(
+      /* (re:MediaFile[])=>{this.videoUrl=re[0].fullPath, */
+      (data: MediaFile[]) => this.videoUrl=data[0].fullPath,
+      (err: CaptureError) => console.error(err)
+      );
+  }
 }
